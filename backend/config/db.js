66438d96321d2b15b186831aa7ec6201414a47fa
Jayson Balatong <jayson.balatong@dxc.com>
2025-09-06 03:35:39 +0800

    Fix server.js in the backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/taskmanager";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/taskmanager", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGO_URI, {});
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
